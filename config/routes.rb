b04a2e7bd1afa4d20dc05ed7f0bac2d66ea8b224
jiaxin xu <jiaxinxu0614@gmail.com>
2022-07-16 13:12:21 +0800

    addviewsfor-post

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
   resources :posts
 
   get 'users/:id/my_posts', to: 'post#my_posts', as: 'my_posts'
-=======
 
-  get 'users/:id/my_posts', to: 'posts#my_posts', as: 'my_posts'
-
-
->>>>>>> d5c95af357500df7fc80315e72a439ba4534b31c
 end
