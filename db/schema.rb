8b7d1d8e11eadf1962998b372efb68495f6b5d4e
erik b <erik.be@outlook.com>
2022-07-16 15:39:49 +0800

    sd

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2022_07_16_072920) do
-
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,10 +50,10 @@ ActiveRecord::Schema.define(version: 2022_07_16_072920) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "title"
     t.string "description"
-    t.bigint "user_id"
-    t.index ["user_id"], name: "index_posts_on_user_id"
     t.string "original_url"
     t.string "original_author"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
