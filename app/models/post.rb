a153cd331928e83349764d67889d265abdd145a5
erik b <erik.be@outlook.com>
2022-08-02 21:55:40 +0800

    adding webmentions

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,17 @@
+require 'json'
+require 'open-uri'
+
 class Post < ApplicationRecord
   belongs_to :user
   enum status: [:public, :followers, :private], _prefix: :posts
+
+  def webmentions
+    url = "https://webmention.io/api/mentions.jf2?domain=www.personalspace.cc/posts/#{self.id}/&token=-T39nR860VEVQhF62w6hjQ
+    &per-page=10"
+    p "======================"
+    response = URI.open(url).read
+    p JSON.parse(response)
+    p "======================"
+  end
+
 end
